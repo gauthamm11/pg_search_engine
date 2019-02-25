54b7f8fdd3c4030c0ac4ce2ff88da31bff9573f9
Gautham Asok <gauthamasok.dev@gmail.com>
2019-02-25 16:29:10 +0530

    updated code

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ require "conn.php";
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"> -->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/velocity/2.0.3/velocity.min.js"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -17,38 +19,38 @@ require "conn.php";
 <div class="bg-light container-fluid">
 	<img class="img-fluid pt-5 mx-auto d-block" src="img/1.jpg" width="200px" height="auto" alt="logo">
 </div>
-<!-- form open -->
 <!-- nav open -->
-<form action="search.php" method="GET">
 <nav class="navbar navbar-expand-sm bg-light justify-content-center sticky-top">
-	<!-- <a class="navbar-brand" href="#!">PG seeKing</a> -->
-	
-	<div class="input-group mt-4 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 shadow-lg p-0 mb-4 bg-light">
+	<form action="search.php"  class="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12" method="GET">
+	<div class="input-group mt-4 shadow-lg p-0 mb-4 bg-light">
+
 		<div class="input-group-prepend">
-			<button class="btn btn-outline-info " type="button">Chennai</button> 
+			<button class="btn btn-info" type="button"><strong>Chennai</strong></button> 
 		</div>
 		<input type="text" name="search" id="search"  placeholder="Enter Area, Street" class="form-control form-control-lg text-capitalize text-monospace" required autofocus/>
-		
 		<div class="input-group-append">
-			<button class="btn btn-info" id="subm" type="submit">Search</button> 
+			<button class="btn btn-info" id="subm" type="submit"><img src="img/search.png" class="img-fluid"></button> 
 		</div>
 	</div>
-	<!-- <a class="navbar-brand text-light" href="#!">PG seeKing</a> -->
-	<!-- <span class="text-light">PG seeKing</span> -->
+	</form>
 </nav>
-</form>
 <!-- nav close -->
-<!-- form close -->
 
+<!-- main content start -->
+<div class="container-fluid text-center">
+<?php
 
-<div class="container-fluid">
+$i = 1;
+for($i = 1; $i<=100; $i++){
+	echo $i."<hr>";
+}
 
+?>
 </div>
+<!-- main content finish -->
 
 <script>
-// jquery open
 $(document).ready(function () {
-//	$("#errd").hide();
 // (getting top) of page open
 var distance = $('#nid').offset().top;
 $(window).scroll(function() {
