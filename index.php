0e6f7589d7d5e9be78caa451cf542966ac2bba4b
Gautham Asok <gauthamasok.dev@gmail.com>
2019-02-25 06:45:38 +0530

    finished stable search query

--- index.php
+++ index.php
@@ -14,32 +14,27 @@ require "conn.php";
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <body class="container-fluid p-0" class="text-justify">
-<style>
-/*	input::-webkit-calendar-picker-indicator {
-  display: none;
-}*/
-</style>
 <div class="bg-light container-fluid">
-	<img class="img-fluid mx-auto d-block" src="img/1.jpg" width="200px" height="auto" alt="logo">
+	<img class="img-fluid pt-5 mx-auto d-block" src="img/1.jpg" width="200px" height="auto" alt="logo">
 </div>
 <!-- form open -->
 <!-- nav open -->
 <form action="search.php" method="GET">
 <nav class="navbar navbar-expand-sm bg-light justify-content-center sticky-top">
-	<a class="navbar-brand" href="#!">PG seeKing</a>
+	<!-- <a class="navbar-brand" href="#!">PG seeKing</a> -->
 	
-	<div class="input-group mt-3 mb-3 col-xs-12 col-lg-6 col-md-12 col-sm-12">
+	<div class="input-group mt-4 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12 shadow-lg p-0 mb-4 bg-light">
 		<div class="input-group-prepend">
-			<button class="btn btn-outline-primary" type="button">Chennai</button> 
+			<button class="btn btn-outline-info " type="button">Chennai</button> 
 		</div>
-		<input type="text" name="search" id="search"  placeholder="Search Area, Street, Pincode" class="form-control form-control-lg text-capitalize text-monospace" required autofocus/>
+		<input type="text" name="search" id="search"  placeholder="Enter Area, Street" class="form-control form-control-lg text-capitalize text-monospace" required autofocus/>
 		
 		<div class="input-group-append">
 			<button class="btn btn-info" id="subm" type="submit">Search</button> 
 		</div>
 	</div>
-	
-	<span class="text-light">PG seeKing</span>
+	<!-- <a class="navbar-brand text-light" href="#!">PG seeKing</a> -->
+	<!-- <span class="text-light">PG seeKing</span> -->
 </nav>
 </form>
 <!-- nav close -->
@@ -54,7 +49,6 @@ require "conn.php";
 // jquery open
 $(document).ready(function () {
 //	$("#errd").hide();
-$(':input[type="submit"]').prop('disabled', true);
 // (getting top) of page open
 var distance = $('#nid').offset().top;
 $(window).scroll(function() {
--- search.php
+++ search.php
@@ -8,7 +8,34 @@ if (!isset($_GET['search'])) {
 $search = mysqli_real_escape_string($conn, $_GET['search']);
 
 $myArray = explode(',', $search);
-print_r($myArray);
-// echo "$search";
+
+$conX = "SELECT * FROM chn_areas WHERE ";
+
+$supp = " OR a_name IN (SELECT a_name FROM chn_areas WHERE";
+
+$conY = "";
+$conZ = "";
+$or = "";
+
+// query v1.1 stable open
+// SELECT * FROM chn_areas WHERE ( (soundex(a_name) LIKE soundex('t nagar')) OR (soundex(a_sub) LIKE soundex('t nagar')) )OR ( (soundex(a_name) LIKE soundex(' usman')) OR (soundex(a_sub) LIKE soundex(' usman')) ) OR a_name IN (SELECT a_name FROM chn_areas WHERE (a_name LIKE '%t nagar%') OR (a_sub LIKE '%t nagar%') OR (a_name LIKE '% usman%') OR (a_sub LIKE '% usman%') ) ORDER BY a_rating;
+// query v1.1 stable close
+
+foreach($myArray as $my_Array){
+	// ((a_name like '%north%') or (a_name like '%usman%'))
+	// select * from chn_areas where (soundex(a_name) like soundex('poruur'))
+	if( !next( $myArray ) ) {
+        $or = "";
+    }else {
+    	$or = "OR";
+    }
+	$conY .= "( (soundex(a_name) LIKE soundex('".$my_Array."')) OR (soundex(a_sub) LIKE soundex('".$my_Array."')) )".$or." ";
+	$conZ .= " (a_name LIKE '%".$my_Array."%') OR (a_sub LIKE '%".$my_Array."%') ".$or." ";
+
+}
+
+$searchQ = $conX.$conY.$supp.$conZ.") ORDER BY a_rating";
+
+echo $searchQ;
 
 ?>
